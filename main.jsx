266e5e62f02867a1cea6f8cd7aaa6646d4f57dcb
ceeeelinnnee05 <empigcelinejynn@gmail.com>
2025-04-13 20:02:25 +0800

    Update main.jsx
    git add main.jsx git commit -m "Fix duplicate code in main.jsx" git push origin main

--- main.jsx
+++ main.jsx
@@ -12,18 +12,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </ErrorBoundary>
   </React.StrictMode>
 );
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import ErrorBoundary from "./ErrorBoundary";
-
-// Render the application inside the ErrorBoundary
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </React.StrictMode>
-);
