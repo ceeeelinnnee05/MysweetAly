51ff1fa2506c3608859e0d1f8b14bc5ba12a7a18
ceeeelinnnee05 <empigcelinejynn@gmail.com>
2025-04-13 20:00:51 +0800

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,9 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import ErrorBoundary from "./ErrorBoundary";
 
+// Render the application inside the ErrorBoundary
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
+);
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import ErrorBoundary from "./ErrorBoundary";
+
+// Render the application inside the ErrorBoundary
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
